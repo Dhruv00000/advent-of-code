15f8506ea38cc66b1e4c236473bd048be6a75975
Dhruv <dhruvmitna9@gmail.com>
2023-11-10 15:59:24 +0530

    2015-2

---
+++ 2015/2/2.py
@@ -0,0 +1,35 @@
+with open("input", "r") as text:
+    inputText = text.readlines()
+
+totalPaperRequirement, totalRibbonRequirement = 0, 0
+for box in inputText:
+    dimensions = box.split("x")
+
+    tsa = 2 * (
+        int(dimensions[0]) * int(dimensions[1])
+        + int(dimensions[1]) * int(dimensions[2])
+        + int(dimensions[2]) * int(dimensions[0])
+    )
+    paperRequirement = tsa + min(
+        [
+            (int(dimensions[0]) * int(dimensions[1])),
+            (int(dimensions[1]) * int(dimensions[2])),
+            (int(dimensions[2]) * int(dimensions[0])),
+        ]
+    )
+    totalPaperRequirement += paperRequirement
+
+    vol = int(dimensions[1]) * int(dimensions[2]) * int(dimensions[0])
+    ribbonRequirement = vol + min(
+        [
+            2 * (int(dimensions[0]) + int(dimensions[1])),
+            2 * (int(dimensions[1]) + int(dimensions[2])),
+            2 * (int(dimensions[2]) + int(dimensions[0])),
+        ]
+    )
+    totalRibbonRequirement += ribbonRequirement
+
+print(totalPaperRequirement)
+print(totalRibbonRequirement)
+
+text.close()
